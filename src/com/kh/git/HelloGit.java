200ee54aabf0c76625da00a0f8ee0a2aad6abe15
YukyungShinn <syk6715@naver.com>
2022-06-15 13:42:00 +0900

    Cat기능 추가

--- src/com/kh/git/HelloGit.java
+++ src/com/kh/git/HelloGit.java
@@ -1,5 +1,6 @@
 package com.kh.git;
 
+import com.kh.git.animal.Cat;
 import com.kh.git.animal.Dog;
 
 public class HelloGit {
@@ -10,6 +11,10 @@ public class HelloGit {
 		//Dog 기능 추가
 		Dog dog = new Dog();
 		dog.bark();
+		
+		//Cat 기능 추가
+		Cat cat = new Cat();
+		cat.miao();
 	}
 	
 }
