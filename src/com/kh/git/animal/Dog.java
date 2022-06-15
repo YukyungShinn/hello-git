577f5846f662d1b1e4b6141e74147b3381e572a8
YukyungShinn <syk6715@naver.com>
2022-06-15 14:08:57 +0900

    bread 필드 추가

--- src/com/kh/git/animal/Dog.java
+++ src/com/kh/git/animal/Dog.java
@@ -2,8 +2,10 @@ package com.kh.git.animal;
 
 public class Dog {
 	private String name;
+	private String breed; // animal branch에서 추가
 	
 	public void bark() {
 		System.out.println("멍멍!");
+		System.out.println("왈왈");
 	}
 }
